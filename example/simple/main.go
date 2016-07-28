d6f9d9e6be7e563e1cbbbdce44c2986d3c27a1e7
warmans <stefan.warman@gmail.com>
2016-07-28 20:18:59 +0200

    Added support for handlers with context. Added example. Added readme.

---
+++ example/simple/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"log"
+
+	"github.com/gorilla/mux"
+	"github.com/warmans/route-rest/routes"
+	"golang.org/x/net/context"
+)
+
+//Simple handler just implements a few verbs and prints some data from the request/context
+type SimpleHandler struct {
+	//include default handlers so the struct implements RESTCtxHandler
+	routes.DefaultRESTCtxHandler
+	//since all routes will use the same handler provider a per-instance prefix to be printed to the response
+	responsePrefix string
+}
+
+//Init is called due to the InitCtx middleware for all requests. If next() is not invoked the request
+//will stop here.
+func (h *SimpleHandler) Init(rw http.ResponseWriter, r *http.Request, next routes.CtxHandleFunc) {
+
+	//abort the request if the form is unparsable
+	if err := r.ParseForm(); err != nil {
+		http.Error(rw, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	//example of aborting based on some user input e.g. http://localhost:8080/api/foo?abort=1
+	if r.Form.Get("abort") != "" {
+		http.Error(rw, "aborted", http.StatusOK)
+		return
+	}
+
+	//invoke the verb handler with extra stuff in the context
+	next(rw, r, context.WithValue(context.Background(), "somekey", "somevalue"))
+
+	//always close the request body
+	r.Body.Close()
+}
+
+//HandleGet is called for GET http://localhost:8080/api/foo/1 <- with ID
+func (h *SimpleHandler) HandleGet(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	fmt.Fprintf(rw, "%s:%s:%+v:%s", h.responsePrefix, "GET", mux.Vars(r), ctx.Value("somekey").(string))
+}
+
+//HandleGetList is called for GET http://localhost:8080/api/foo <-- without ID
+func (h *SimpleHandler) HandleGetList(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	fmt.Fprintf(rw, "%s:%s:%+v", h.responsePrefix, "GET LIST", mux.Vars(r))
+}
+
+//HandleGetList is called for POST http://localhost:8080/api/foo <-- without ID
+func (h *SimpleHandler) HandlePost(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	fmt.Fprintf(rw, "%s:%s:%+v", h.responsePrefix, "POST", mux.Vars(r))
+}
+
+func main() {
+
+	//start with a new muxRouter
+	router := mux.NewRouter()
+
+	//apply the REST routes. This will create the following routes:
+	// /foo
+	// /foo/1
+	// /foo/1/bar
+	// /foo/1/bar/1
+	// /foo/1/baz
+	// /foo/1/baz/1
+	routes.ApplyRoutes(
+		router,
+		[]*routes.Route{
+			routes.NewRoute(
+				"foo",                                                 //resource name
+				"{foo_id:[0-9]}",                                      //id pattern
+				routes.InitCtx(&SimpleHandler{responsePrefix: "foo"}), //handler
+				[]*routes.Route{ //sub-routes
+					routes.NewRoute(
+						"bar",
+						"{bar_id:[0-9]}",
+						routes.InitCtx(&SimpleHandler{responsePrefix: "bar"}),
+						[]*routes.Route{},
+					),
+					routes.NewRoute(
+						"baz",
+						"{baz_id:[0-9]}",
+						routes.InitCtx(&SimpleHandler{responsePrefix: "baz"}),
+						[]*routes.Route{},
+					),
+				},
+			),
+		},
+		[]string{""}, //no prefix on root resource
+	)
+
+	//attach it to a http mux with a prefix
+	mux := http.NewServeMux()
+	mux.Handle("/api/", http.StripPrefix("/api", router))
+
+	//serve the mux
+	log.Println("Listening on :8080...")
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
--- routes/rest.go
+++ routes/rest.go
@@ -2,8 +2,63 @@ package routes
 
 import (
 	"net/http"
+
+	"golang.org/x/net/context"
 )
 
+type CtxHandleFunc func(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+
+type RESTCtxHandler interface {
+	Init(rw http.ResponseWriter, r *http.Request, next CtxHandleFunc)
+	HandleGet(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandleGetList(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandlePost(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandlePut(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandlePatch(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandleDelete(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandleCopy(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandleHead(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandleOptions(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+}
+
+// -----------------------
+// default handler. embed this in a handler to follow the RESTCtxHandler interface without needing to implement all the
+// verbs
+
+type DefaultRESTCtxHandler struct{}
+
+//Init creates the context passed to each handler. Overriding this allows population of the context for all
+//verbs
+func (h *DefaultRESTCtxHandler) Init(rw http.ResponseWriter, r *http.Request, next CtxHandleFunc) {
+	next(rw, r, context.Background())
+}
+func (h *DefaultRESTCtxHandler) HandleGet(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTCtxHandler) HandleGetList(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTCtxHandler) HandlePost(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTCtxHandler) HandlePut(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTCtxHandler) HandlePatch(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTCtxHandler) HandleDelete(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTCtxHandler) HandleCopy(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTCtxHandler) HandleHead(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTCtxHandler) HandleOptions(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
 
 type RESTHandler interface {
 	HandleGet(rw http.ResponseWriter, r *http.Request)
@@ -18,19 +73,17 @@ type RESTHandler interface {
 }
 
 // -----------------------
-// default handler. embed this in your handler to provide defaults for all the verbs.
+// default handler. embed this in a handler to follow the RESTHandler interface without needing to implement all the
+// verbs
 
 type DefaultRESTHandler struct{}
-
 
 func (h *DefaultRESTHandler) HandleGet(rw http.ResponseWriter, r *http.Request) {
 	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
 }
-
 func (h *DefaultRESTHandler) HandleGetList(rw http.ResponseWriter, r *http.Request) {
 	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
 }
-
 func (h *DefaultRESTHandler) HandlePost(rw http.ResponseWriter, r *http.Request) {
 	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
 }
@@ -52,4 +105,3 @@ func (h *DefaultRESTHandler) HandleHead(rw http.ResponseWriter, r *http.Request)
 func (h *DefaultRESTHandler) HandleOptions(rw http.ResponseWriter, r *http.Request) {
 	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
 }
-
